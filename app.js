d7213fea057fb5b9a01f2a31cbec0fb30c065e12
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-02-13 19:25:04 +0600

    Second commit

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncreasing) {
+        productNumber = parseInt(productNumber) + 1;
+        productInput.value = productNumber;
+    }
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
+        productInput.value = productNumber;
+    }
+    //update product total
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+}
+//Handle case increase decrease events
 document.getElementById('case-plus').addEventListener('click', function () {
-    const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
+    updateProductNumber('case', 59, true);
+})
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateProductNumber('case', 59, false);
+})
+//handling phone increase decrease events
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true);
+})
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false);
 })
